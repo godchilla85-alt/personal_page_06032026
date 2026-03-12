683846122323b2672a77f97d33685fe027f13a99
godchilla85-alt <godchilla85@gmail.com>
2026-03-12 11:46:29 +0100

    added resume link

--- src/components/AboutPageMe.vue
+++ src/components/AboutPageMe.vue
@@ -10,7 +10,7 @@
             <p class="text-4xl pr-0 lg:pr-10">Moinsen. Ich, Programmierer mit über 6 Jahren Erfahrung und einer großen Leidenschaft für Computer und die Technik dahinter. Mein Interesse begann bereits mit 15 Jahren, als ich angefangen habe, mich intensiver mit Computern zu beschäftigen. Seitdem habe ich unzählige Stunden auch privat investiert, um neue Technologien, Programmiersprachen und Programme zu lernen und mein Wissen ständig zu erweitern.
 
 In meiner Freizeit spiele ich gerne Games, verbringe Zeit mit meinem Familie, höre Musik oder fiebere bei Spielen des Hamburger SV mit. Manchmal genieße ich es aber auch einfach, zu entspannen und abzuschalten.</p>
-                <AnimatedLink text="Mein Resume"></AnimatedLink>
+                <AnimatedLink text="Mein Resume"  href="https://www.sebastian-pohl.eu/Resume_Sebastian_Pohl.pdf"></AnimatedLink>
        
         </div>
         <div class="w-full relative z-50 mb-10 order-1 lg:w-1/2 lg:order-2"
