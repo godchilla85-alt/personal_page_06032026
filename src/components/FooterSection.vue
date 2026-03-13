b820318a84f82c40fed8c575da944deb3e76f035
godchilla85-alt <godchilla85@gmail.com>
2026-03-13 12:34:56 +0100

    footer section padding and margin fix

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -5,13 +5,13 @@ Aos.init({ once: true });
 </script>
 
 <template>
-    <div class="relative min-h-[20vh] container bg-white/10 rounded-t-4xl backdrop-blur-sm shadow-xl pt-20"
+    <div class="relative min-h-[20vh] container bg-white/10 rounded-t-4xl backdrop-blur-sm shadow-xl pt-10"
     data-aos='fade-down'
             data-aos-duration="2000"
             data-aos-delay="50">
         
         
-        <div class="h-full flex flex-col p-10 relative z-10">
+        <div class="h-full flex flex-col px-10 py-5 relative z-10">
             
             <div class="grow flex flex-col justify-center">
                 <h3 class="text-4xl text-white/70">Überzeugt?</h3>
@@ -20,7 +20,7 @@ Aos.init({ once: true });
                 </a>
             </div>
 
-            <div class="mb-1.25 pt-30">
+            <div class="pt-15">
                 <FooterCopyright />
             </div>
             
